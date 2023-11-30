01d6f6b5ef13068224c40e541f5d1efb068b4a2a
msulejczak <sulej1@wp.pl>
2023-11-30 16:02:40 +0100

    Szblony klas C++

---
+++ Programowanie obiektowe/Szablony funkcji i klas/SzablonyKlas/Specjalizowane/Specjalizowane.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+template <class T>
+
+class Oceny {
+public:
+    Oceny()
+    {
+        cout << "konstruktor klasy ogólnej...." << endl;
+    }
+
+    T wyklad, cwiczenia, laboratorium, seminarium;
+
+    T srednia() {
+        return (wyklad + cwiczenia + laboratorium + seminarium) / 4;
+    }
+};
+
+template <> class
+Oceny<double> {
+public:
+    Oceny()
+    {
+        cout << "konstruktor klasy specjalizowanej...." << endl;
+    }
+
+    double wyklad, cwiczenia, laboratorium, seminarium;
+
+    double srednia() {
+        return (round(wyklad) + round(cwiczenia) + round(laboratorium) + round(seminarium)) / 4;
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Oceny<int> oceny1 = Oceny<int>();
+
+    oceny1.wyklad = 3;
+    oceny1.cwiczenia = 4;
+    oceny1.laboratorium = 5;
+    oceny1.seminarium = 3;
+
+    cout << "Średnia ocen semestralnych:" << oceny1.srednia() << endl;
+
+    Oceny<double> oceny2 = Oceny<double>();
+
+    oceny2.wyklad = 3.0;
+    oceny2.cwiczenia = 4.0;
+    oceny2.laboratorium = 5.0;
+    oceny2.seminarium = 3.0;
+
+    cout << "Średnia ocen semestralnych:" << oceny2.srednia() << endl;
+}
