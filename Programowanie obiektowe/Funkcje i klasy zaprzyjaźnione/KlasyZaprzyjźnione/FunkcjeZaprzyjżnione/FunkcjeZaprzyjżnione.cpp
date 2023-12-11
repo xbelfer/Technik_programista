5751ed09175a50e93d43301eb2a0a95618f4ece8
xbelfer <sulej1@wp.pl>
2023-12-11 20:47:17 +0100

    szablonowy bubble sort

---
+++ Programowanie obiektowe/Funkcje i klasy zaprzyjaźnione/KlasyZaprzyjźnione/FunkcjeZaprzyjżnione/FunkcjeZaprzyjżnione.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+class Pracownik;
+
+string infoPracownik(const Pracownik *pr);
+
+class Pracownik
+{
+private: 
+    string imie;
+    string nazwisko;
+    string stanowisko;
+    int placa;
+public:
+    Pracownik(string imie, string nazwisko, string stanowisko, int placa)
+    {
+        this->imie = imie;
+        this->nazwisko = nazwisko;
+        this->stanowisko = stanowisko;
+        this->placa = placa;
+    }
+    friend string infoPracownik(const Pracownik *pr);
+};
+
+string infoPracownik(const Pracownik *pr)
+{
+    string ret = "\nImie: " + pr->imie;
+    ret += "\nNazwisko: " + pr->nazwisko;
+    ret += "\nStanowisko: " + pr->stanowisko;
+    ret += "\nPłaca: " + to_string(pr->placa);
+    return ret;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    Pracownik p1("Jan", "Kowalski", "magazynier", 2400);
+
+    cout<<infoPracownik(&p1);
+}
+
---
+++ Programowanie obiektowe/Szablony funkcji i klas/SzablonyFunkcji/BubleSort/BubleSort.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void BubleSort(T *tab, int size)
+{
+    for (int j = 0; j < size - 1; j++)
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (tab[i] > tab[i + 1])
+            {
+                T temp = tab[i];
+                tab[i] = tab[i+1];
+                tab[i + 1] = temp;
+            }
+        }
+    }
+}
+
+template <typename T>
+void DisplayTab(T* tab, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << tab[i] << ", ";
+    }
+    cout << endl;
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int tab[6]{9,4,7,1,6,3};
+
+    DisplayTab<int>(tab, 6);
+    BubleSort<int>(tab, 6);
+    DisplayTab<int>(tab, 6);
+
+}
