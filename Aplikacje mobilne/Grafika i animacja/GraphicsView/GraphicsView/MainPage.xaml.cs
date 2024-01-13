d42cd4532daa82cb02accdcdc286697a2c8b4dd2
xbelfer <sulej1@wp.pl>
2024-01-13 12:30:52 +0100

    Grafika i animacja w MAUI

--- Aplikacje mobilne/Grafika i animacja/GraphicsView/GraphicsView/MainPage.xaml.cs
+++ Aplikacje mobilne/Grafika i animacja/GraphicsView/GraphicsView/MainPage.xaml.cs
@@ -2,12 +2,17 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        int i = 0;
         public MainPage()
         {
             InitializeComponent();
         }
 
+		private void ClickElipse(object sender, TouchEventArgs e)
+		{
+            ++i;
+            lbElipse.Text = $"Clicked Elipse {i}";
+        }
     }
 
 }
