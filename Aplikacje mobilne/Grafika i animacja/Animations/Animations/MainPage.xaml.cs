d89568e5f244a8c3427e255176a9039234badc5c
xbelfer <sulej1@wp.pl>
2024-01-14 20:06:48 +0100

    modyfikacja aplikacji z animacją

--- Aplikacje mobilne/Grafika i animacja/Animations/Animations/MainPage.xaml.cs
+++ Aplikacje mobilne/Grafika i animacja/Animations/Animations/MainPage.xaml.cs
@@ -2,24 +2,47 @@
 {
     public partial class MainPage : ContentPage
     {
-        public static double count = 32;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count+=5;
+            await image.FadeTo(0);
+            await Task.Delay(1000);
+			await image.FadeTo(1);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+			await image.ScaleTo(0, easing: Easing.SpringIn);
+			await Task.Delay(1000);
+			await image.ScaleTo(1, easing: Easing.SpringOut);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+            await image.RotateTo(180, 500, Easing.CubicInOut);
+			await Task.Delay(1000);
+			await image.RotateTo(360, 500, Easing.CubicInOut);
+			await Task.Delay(1000);
+			image.Rotation = 0;
+
+			await image.TranslateTo(0, -50, 250, Easing.CubicInOut);
+			await Task.Delay(1000);
+			await image.TranslateTo(0, 0, 250, Easing.CubicInOut);
+			await Task.Delay(1000);
+			
+
+			await Task.WhenAny(
+                image.RotateTo(360, 500, Easing.CubicInOut),
+                image.TranslateTo(0, -50, 250, Easing.CubicInOut)
+            );
+			await image.TranslateTo(0, 0, 250, Easing.CubicInOut);
+			image.Rotation = 0;
+
+			await CounterBtn.RotateTo(180, 500, Easing.CubicInOut);
+			await Task.Delay(1000);
+			await CounterBtn.RotateTo(360, 500, Easing.CubicInOut);
+			await Task.Delay(1000);
+			CounterBtn.Rotation = 0;
+		}
     }
 
 }
